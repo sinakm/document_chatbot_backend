ee0ac35f2c40e7347efa004022be024e4b73f41d
mohdreyhan <mdreyhan007@gmail.com>
2024-07-21 09:50:56 -0400

    initial commit - project setup

---
+++ test/document_chatbot_backend.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const DocumentChatbotBackend = require('../lib/document_chatbot_backend-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/document_chatbot_backend-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new DocumentChatbotBackend.DocumentChatbotBackendStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
